36911a712f0fd7fed656b2f25cb76b169b1a021f
Brian Anderson <andersrb@gmail.com>
2021-03-18 21:06:58 -0400

    .

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 #![feature(decl_macro, proc_macro_hygiene)]
 
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate include_dir;
+
+use std::path::PathBuf;
+use anyhow::{Result, anyhow};
+use include_dir::Dir;
+use log::info;
+use rocket::response::content::Html;
+
+static STATIC_FILES: Dir = include_dir!("./static");
 
 #[get("/api/create")]
 fn create_treasure_key() -> String {
@@ -18,8 +27,17 @@ fn claim_treasure_with_key() -> String {
 }
 
 #[get("/")]
-fn static_root() -> String {
-    "hi".to_string()
+fn static_root() -> Html<String> {
+    let file = STATIC_FILES.get_file("index.html").unwrap();
+    Html(file.contents_utf8().unwrap().to_string())
+}
+
+#[get("/<path..>")]
+fn static_file(path: PathBuf) -> Result<String> {
+    let file = STATIC_FILES.get_file(path)
+        .ok_or_else(|| anyhow!("not found"))?;
+
+    Ok(file.contents_utf8().unwrap().to_string())
 }
 
 fn main() {
@@ -29,6 +47,7 @@ fn main() {
             plant_treasure_with_key,
             claim_treasure_with_key,
             static_root,
+            static_file,
         ])
         .launch();
 }
