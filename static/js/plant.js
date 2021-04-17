5b965ab79a6e144fda9368ae4e8db15fc563e994
Brian Anderson <andersrb@gmail.com>
2021-04-17 17:15:12 -0400

    .

--- static/js/plant.js
+++ static/js/plant.js
@@ -2,20 +2,6 @@ console.log("before click");
 
 let treasureImageBin = null;
 
-let plantButton = document.getElementById("plant-button");
-
-console.log(plantButton);
-
-plantButton.addEventListener("click", async () => {
-    console.log("click");
-
-    let response = await fetch("api/plant");
-    console.log(response);
-
-    let jsonResponse = await response.json();
-    console.log(jsonResponse);
-});
-
 let imageUploadButton = document.getElementById("image-upload-button");
 
 imageUploadButton.addEventListener("change", async () => {
@@ -32,4 +18,42 @@ imageUploadButton.addEventListener("change", async () => {
     let blob = new Blob([bin], { type: file.type });
 
     imgElt.src = URL.createObjectURL(blob);
+});
+
+let qrScanButton = document.getElementById("qrscan-button");
+
+QrScanner.WORKER_PATH = "js/lib/qr-scanner-worker.min.js";
+
+qrScanButton.addEventListener("click", async () => {
+    qrScanButton.enabled = false;
+    let video = document.getElementById("qr-video");
+    let stopScanning = null;
+    const qrScanner = new QrScanner(video, (result) => {
+        console.log(result);
+        stopScanning();
+
+        
+    }, (error) => {
+        //console.error(error);
+        //stopScanning();
+    });
+    stopScanning = () => {
+        qrScanner.stop();
+        qrScanner.destroy();
+        qrScanButton.enabled = true;
+    }
+    qrScanButton.enabled = false;
+    qrScanner.start();
+});
+
+let plantButton = document.getElementById("plant-button");
+
+plantButton.addEventListener("click", async () => {
+    console.log("click");
+
+    let response = await fetch("api/plant");
+    console.log(response);
+
+    let jsonResponse = await response.json();
+    console.log(jsonResponse);
 });
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -12,3 +12,13 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 pub fn set_panic_hook() {
     utils::set_panic_hook();
 }
+
+pub struct KeyInfo {
+    secret: String,
+    public: String,
+}
+
+#[wasm_bindgen]
+pub fn secret_url_to_key_info(url: &str) -> KeyInfo {
+    panic!()
+}
