9754f0eb4259e238d48a4b15693b0931c74d5201
Brian Anderson <andersrb@gmail.com>
2021-04-24 10:23:57 -0400

    Cleanup create page/script

--- static/js/create.js
+++ static/js/create.js
@@ -1,15 +1,31 @@
-console.log("before click");
 let createButton = document.getElementById("create-button");
-console.log(createButton);
+let qrCodeContainer = document.getElementById("display-qrcode");
+let secretKeyContainer = document.getElementById("secret-key");
+
+console.assert(createButton);
+console.assert(qrCodeContainer);
+console.assert(secretKeyContainer);
+
 createButton.addEventListener("click", async () => {
-    console.log("click");
+    qrCodeContainer.innerHTML = null;
+    secretKeyContainer.innerHTML = null;
+    createButton.disabled = true;
 
-    let response = await fetch("api/create");
-    console.log(response);
+    try {
+        let response = await fetch("api/create");
 
-    let jsonResponse = await response.json();
-    console.log(jsonResponse);
+        console.log(response);
 
-    document.getElementById("display-qrcode").innerHTML = jsonResponse.qrcode;
+        let jsonResponse = await response.json();
+
+        console.log(jsonResponse);
+        console.assert(jsonResponse.qrcode);
+        console.assert(jsonResponse.secret_key);
+
+        qrCodeContainer.innerHTML = jsonResponse.qrcode;
+        secretKeyContainer.innerHTML = jsonResponse.secret_key;
+    } finally {
+        createButton.disabled = false;
+    }
 });
 
