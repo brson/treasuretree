b9211e54a66fe64668b3777605b1ebc6dc6cd0e5
Brian Anderson <andersrb@gmail.com>
2021-05-19 19:58:06 -0400

    Add geonft_shared and outline sync-bot

--- src/sync-bot/src/main.rs
+++ src/sync-bot/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused)]
+
+use anyhow::Result;
+use log::info;
+
+fn main() -> Result<()> {
+    env_logger::init();
+
+    loop {
+        let plan = make_plan()?;
+        execute_plan(plan)?;
+    }
+}
+
+struct Plan {
+    steps: Vec<Step>,
+}
+
+enum Step {
+    UploadTreasureToIpfs {
+    },
+    UploadPlantToSolana {
+    },
+    UploadClaimToSolana {
+    },
+}
+
+fn make_plan() -> Result<Plan> {
+    info!("making new plan");
+    panic!()
+}
+
+fn execute_plan(plan: Plan) -> Result<()> {
+    info!("executing plan");
+    panic!()
 }
