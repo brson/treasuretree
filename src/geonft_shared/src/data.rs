c3f072dac394ce8c599f2af9720b1654770061e1
Brian Anderson <andersrb@gmail.com>
2021-05-19 20:40:40 -0400

    .

--- src/geonft_shared/src/data.rs
+++ src/geonft_shared/src/data.rs
@@ -1,2 +1,13 @@
+use anyhow::Result;
+
 pub static PLANT_DIR: &'static str = "data/plant";
 pub static CLAIM_DIR: &'static str = "data/claim";
+
+pub struct PlantedTreasure {
+    pub public_key: String,
+    pub time: chrono::DateTime::<chrono::Local>,
+}
+
+pub fn get_all_planted_treasures() -> Result<Vec<String>> {
+    todo!()
+}
