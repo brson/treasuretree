99022747ae904ef28176c1c4e21b11ae1104a889
Brian Anderson <andersrb@gmail.com>
2021-06-02 23:54:13 +0000

    .

---
+++ build-all-release.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+wasm-pack build --target=web src/geonft_wasm
+cargo build-bpf --manifest-path=src/geonft_solana/Cargo.toml
+cargo build -p geonft --release
+cargo build -p geonft_sync --release
